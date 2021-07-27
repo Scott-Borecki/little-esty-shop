9311f88ee6c19605e647562912b739218d57f45b
Caroline Tan <carolinectan3@gmail.com>
2021-07-26 23:26:01 -0400

    Refactor tests

--- spec/models/items.rb
+++ spec/models/items.rb
@@ -11,7 +11,7 @@ RSpec.describe Item, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:unit_price) }
-    it { should validate_presence_of(:boolean) }
+    it { should validate_presence_of(:enabled) }
   end
 
   # before :each do
