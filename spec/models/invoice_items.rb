4101f1c8fdf6aecba7ee9da0407313d05411ebd8
Caroline Tan <carolinectan3@gmail.com>
2021-07-26 23:23:00 -0400

    Add InvoiceItem model relationships and validation tests

---
+++ spec/models/invoice_items.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe InvoiceItem, type: :model do
+  describe 'relationships' do
+    it { should belong_to(:invoice) }
+    it { should belong_to(:item) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:quantity) }
+    it { should validate_presence_of(:unit_price) }
+    it { should validate_presence_of(:status) }
+  end
+
+  # before :each do
+  #
+  # end
+  #
+  # describe 'class methods' do
+  #   describe '.' do
+  #   end
+  # end
+  #
+  # describe 'instance methods' do
+  #   describe '#' do
+  #   end
+  # end
+end
