aea823999b159f351d78761e7f7155841b480976
Caroline Tan <carolinectan3@gmail.com>
2021-07-27 00:08:39 -0400

    Add InvoiceItem validations and relationships

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,3 +1,8 @@
 class InvoiceItem < ApplicationRecord
+  validates_presence_of :quantity
+  validates_presence_of :unit_price
+  validates_presence_of :status
 
+  belongs_to :invoice
+  belongs_to :item
 end
