a3f27d5ee05a566112b45fb8053ed274f3395fd0
Caroline Tan <carolinectan3@gmail.com>
2021-07-28 11:50:24 -0400

    Refactors validations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,8 +1,11 @@
 class Invoice < ApplicationRecord
-  validates :status, presence: true
-  enum status: {"in progress": 0, cancelled: 1, completed: 2}
+  enum status: { "in progress": 0, cancelled: 1, completed: 2 }
+
   belongs_to :customer
+  
   has_many :invoice_items
   has_many :transactions
   has_many :items, through: :invoice_items
+
+  validates :status, presence: true
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,10 @@
 class Transaction < ApplicationRecord
-  validates :credit_card_number, presence: true, numericality: true, length: { in: 15..16 }
-  validates :result, presence: true
-  enum result: {failed: 0, success: 1 }
+  enum result: { failed: 0, success: 1 }
+
   belongs_to :invoice
+
+  validates :credit_card_number, presence: true,
+                                 numericality: true,
+                                 length: { in: 15..16 }
+  validates :result, presence: true, numericality: true
 end
