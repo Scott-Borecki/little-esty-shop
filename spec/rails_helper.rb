66da6dde62e29f064eea2ff65618671e3bf85d7e
Caroline Tan <carolinectan3@gmail.com>
2021-07-27 20:06:18 -0400

    Add simplecov filter for spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
 SimpleCov.start
+SimpleCov.add_filter ['spec']
+
 require 'spec_helper'
 
 ENV['RAILS_ENV'] ||= 'test'
