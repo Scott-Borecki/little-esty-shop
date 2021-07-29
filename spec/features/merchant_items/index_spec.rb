78fd2c367369c9d385b761d7c244782175d5912d
Caroline Tan <carolinectan3@gmail.com>
2021-07-28 21:48:31 -0400

    Add can list items for a merchant test #35

---
+++ spec/features/merchant_items/index_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Merchant Items Index Page' do
+  before :each do
+    @merchant_1 = create(:merchant)
+    @merchant_2 = create(:merchant)
+
+    @item_1 = create(:item, enabled: true, merchant: @merchant_1)
+    @item_2 = create(:item, enabled: true, merchant: @merchant_1)
+    @item_3 = create(:item, enabled: true, merchant: @merchant_1)
+    @item_4 = create(:item, enabled: true, merchant: @merchant_2)
+    @item_5 = create(:item, enabled: true, merchant: @merchant_2)
+
+    visit "/merchants/#{@merchant_1.id}/items"
+  end
+
+  it 'can list all of the items for a merchant' do
+    expect(page).to have_content("#{@merchant_1.name}'s Items")
+    expect(page).to have_content(@item_1.name)
+    expect(page).to have_content(@item_2.name)
+    expect(page).to have_content(@item_3.name)
+    expect(page).to_not have_content(@merchant_2.name)
+    expect(page).to_not have_content(@item_4.name)
+    expect(page).to_not have_content(@item_5.name)
+  end
+end
