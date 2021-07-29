0d108fd7ab376189e49142e02f09a50cee742768
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-07-28 20:33:19 -0600

    Create routes for merchants and merchant/merchant_id/invoices

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get '/', to: 'application#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #merchant invoices ---------------------------------
+  resources :merchants do
+    resources :invoices
+  end
 end
