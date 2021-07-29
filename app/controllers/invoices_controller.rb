e9a7db1c9f1ed3fd84eab175f8feb439cf01049d
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-07-28 20:31:11 -0600

    Add index method for invoices controller

---
+++ app/controllers/invoices_controller.rb
@@ -0,0 +1,10 @@
+class InvoicesController < ApplicationController
+
+  def index
+    @merchant = Merchant.find(params[:merchant_id])
+    @invoices = Invoice.select("DISTINCT invoices.*")
+      .joins(invoice_items: :item)
+      .where('items.merchant_id = ?', params[:merchant_id])
+  end
+
+end
