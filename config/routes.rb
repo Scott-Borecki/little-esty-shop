13db9755cbc3e1b11bce9bff6ebcd2734673c0b8
Alex Klick <aklick12@gmail.com>
2021-07-30 16:29:10 -0600

    passes admin invoice show spec - invoice items attrs tests

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :invoices, only: [:index, :show]
+    resources :invocie_items, only: [:update]
   end 
 end
