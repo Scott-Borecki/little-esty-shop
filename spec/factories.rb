ec67f53263fdfcefa46259f283ff794e29013afa
Scott Borecki <scottborecki@gmail.com>
2021-07-30 14:48:14 -0600

    Update factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,34 @@
 FactoryBot.define do
+  # Customer
   factory :customer do
     first_name { Faker::Movies::HarryPotter.unique.spell }
     last_name  { Faker::Movies::HarryPotter.unique.house }
+    #
+    # factory :customer_with_invoices do
+    #   after(:create) do |customer, evaluator|
+    #     create_list(:invoice, evaluator.invoice_count, customer: customer)
+    #   end
+    # end
   end
 
+  # Invoice
   factory :invoice do
     customer
+  end
+
+  factory :invoice_item do
+    quantity { rand(1..10) }
+    unit_price { rand(1_000..2_000) }
+    item
+    invoice
+  end
+
+  # Item
+  factory :item do
+    name { Faker::Book.title }
+    description { Faker::Company.buzzword }
+    unit_price { rand(1_000..2_000) }
+    merchant
   end
 
   # Merchants
@@ -14,12 +37,29 @@ FactoryBot.define do
 
     factory :enabled_merchant do
       enabled { true }
+    #
+    #   factory :merchant_with_revenue_generated do
+    #     transient do
+    #       items_count { 5 }
+    #     end
+    #
+    #     after(:create) do |merchant, evaluator|
+    #       create_list(:item, evaluator.items_count, merchant: merchant)
+    #       # merchant.reload
+    #     end
+    #   end
     end
 
     factory :disabled_merchant do
       enabled { false }
     end
   end
+
+  # Transaction
+  factory :transaction do
+    credit_card_number { Faker::Business.credit_card_number.delete('-') }
+    credit_card_expiration_date { Faker::Business.credit_card_expiry_date }
+    invoice
   end
 end
 
