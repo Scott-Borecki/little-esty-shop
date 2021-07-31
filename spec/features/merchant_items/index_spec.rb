b1a745d394ce323a76813d33af13ddbf1c9c441b
Caroline Tan <carolinectan3@gmail.com>
2021-07-30 22:34:55 -0400

    Update buttons to show/not show if the item enabled is true/false

--- spec/features/merchant_items/index_spec.rb
+++ spec/features/merchant_items/index_spec.rb
@@ -27,30 +27,34 @@ RSpec.describe 'Merchant Items Index Page' do
 
   it 'has a button next to each item name to disable or enable that item' do
     within "#item-#{@item_1.id}" do
-      expect(page).to have_button('Enable')
+      expect(@item_1.enabled).to eq(true)
+      expect(page).to_not have_button('Enable')
       expect(page).to have_button('Disable')
 
       click_on "Disable"
 
       expect(current_path).to eq(merchant_items_path(@merchant_1))
       expect(@item_1.reload.enabled).to eq(false)
+      expect(page).to have_button('Enable')
+      expect(page).to_not have_button('Disable')
 
       click_on "Enable"
 
       expect(current_path).to eq(merchant_items_path(@merchant_1))
       expect(@item_1.reload.enabled).to eq(true)
-    end
-
-    within "#item-#{@item_2.id}" do
-      expect(page).to have_button('Enable')
+      expect(page).to_not have_button('Enable')
       expect(page).to have_button('Disable')
     end
 
-    within "#item-#{@item_3.id}" do
-      expect(page).to have_button('Enable')
-      expect(page).to have_button('Disable')
-    end
-save_and_open_page
+    # within "#item-#{@item_2.id}" do
+    #   expect(page).to have_button('Enable')
+    #   expect(page).to have_button('Disable')
+    # end
+    #
+    # within "#item-#{@item_3.id}" do
+    #   expect(page).to have_button('Enable')
+    #   expect(page).to have_button('Disable')
+    # end
 
     # When I click this button
     # Then I am redirected back to the items index
