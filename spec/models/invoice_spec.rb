8ddb3b4fd9b47b88a2064be652ceef9de51a0e6c
Scott Borecki <scottborecki@gmail.com>
2021-07-31 09:06:26 -0600

    Update Invoice relationships and validations

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Invoice, type: :model do
+  # See /spec/factories.rb for more info on factories created
+  create_factories
+
+  specify { expect(Customer.all.count).to be_positive }
+  specify { expect(Merchant.all.count).to be_positive }
+  specify { expect(Item.all.count).to be_positive }
+  specify { expect(Invoice.all.count).to be_positive }
+  specify { expect(Transaction.all.count).to be_positive }
+  specify { expect(InvoiceItem.all.count).to be_positive }
+
   describe 'relationships' do
     it { should belong_to(:customer) }
     it { should have_many(:invoice_items) }
     it { should have_many(:transactions) }
     it { should have_many(:items).through(:invoice_items) }
+    it { should have_many(:merchants).through(:items) }
   end
 
   describe 'validations' do
@@ -15,10 +26,13 @@ RSpec.describe Invoice, type: :model do
     it { should validate_presence_of(:status) }
   end
 
+  # DELETE? (Scott Borecki): I think we can remove this test?
   it 'can build an invoice with a customer association' do
+    expect(Invoice.all.size).to eq(6)
     customer_with_in_progress_invoices(invoice_count: 2)
-    expect(Invoice.first.status).to eq("in progress")
-    expect(Invoice.all.length).to eq(2)
+
+    expect(Invoice.last.status).to eq("in progress")
+    expect(Invoice.all.size).to eq(8)
   end
 
   # before :each do
