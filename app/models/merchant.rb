7e21296d7a7c94527a89662d32b0cd429f753b20
Caroline Tan <carolinectan3@gmail.com>
2021-07-31 14:05:09 -0400

    Start to work on more AR user stories

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,12 @@ class Merchant < ApplicationRecord
   validates :name, presence: true
 
   has_many :items
+  has_many :invoice_items, through: :items
+  # has_many :invoices, through: :invoice_items
+  # has_many :customers, through: :invoices
+  # has_many :transactions, through: :invoices
+
+  # AR joins temp tables
+  # calulations: that you want to know, not to save
+  # relationship: want to save
 end
