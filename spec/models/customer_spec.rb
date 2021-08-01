6122880178a08c2b068947fade0eaec6c2c4c94c
Scott Borecki <scottborecki@gmail.com>
2021-08-01 14:31:14 -0600

    Add .top_five_by_successful_transactions (method only) (#20)

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,6 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
+  # See /spec/factories.rb for more info on factories created
+  create_factories
+
+  describe 'object creation for tests' do
+    specify { expect(Customer.all.count).to be_positive }
+    specify { expect(Merchant.all.count).to be_positive }
+    specify { expect(Item.all.count).to be_positive }
+    specify { expect(Invoice.all.count).to be_positive }
+    specify { expect(Transaction.all.count).to be_positive }
+    specify { expect(InvoiceItem.all.count).to be_positive }
+
+    it 'can build a customer' do
+      customer_1 = create(:customer)
+
+      expect(customer_1.first_name).to be_a(String)
+      expect(customer_1.last_name).to be_a(String)
+    end
+  end
+
   describe 'relationships' do
     it { should have_many(:invoices) }
   end
@@ -16,12 +35,12 @@ RSpec.describe Customer, type: :model do
     it { should have_many(:merchants).through(:items) }
   end
 
-  # TODO: Add validation to check customer is a Customer instance (See Line 25 below).
-  it 'can build a customer' do
-    customer1 = create(:customer)
-    
-    expect(customer1.first_name).to be_a(String)
-    expect(customer1.last_name).to be_a(String)
-    # expect(customer1).to be_an_instance_of(Customer)
+  describe 'class methods' do
+    describe '.top_five_by_successful_transactions' do
+      it 'returns the customers with the most successful transactions' do
+        expected = [customer2, customer4, customer6, customer5, customer3]
+        expect(Customer.top_five_by_successful_transactions).to eq(expected)
+      end
+    end
   end
 end
