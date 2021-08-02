a17733af213d1bfa5ee15a835f28ac39119a6f67
Scott Borecki <scottborecki@gmail.com>
2021-08-02 11:45:51 -0600

    Refactor tests with let block

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,12 +12,12 @@ RSpec.describe Merchant, type: :model do
     specify { expect(Transaction.all.count).to be_positive }
     specify { expect(InvoiceItem.all.count).to be_positive }
 
-    it 'can build a merchant' do
-      merchant1 = create(:merchant)
+    it 'creates a new merchant' do
+      new_merchant = create(:merchant)
 
-      expect(merchant1).to be_a(Merchant)
-      expect(merchant1.name).to be_a(String)
-      # TODO: Add test for default enabled status.
+      expect(new_merchant).to be_a(Merchant)
+      expect(new_merchant.name).to be_a(String)
+      expect(new_merchant.enabled).to be false
     end
   end
 
@@ -86,46 +86,22 @@ RSpec.describe Merchant, type: :model do
         expect(merchant6.top_revenue_day).to eq("July 27, 2021")
       end
     end
-  end
-
-  # TODO: Update tests to use all the same factories instead of the instance variables.  Combine with 'instance methods' describe block above.
-  describe 'instance methods PART 2' do
-    before :each do
-      # merchants
-      @merchant1 = Merchant.create!(name: 'Joe Schmo')
-      @merchant2 = Merchant.create!(name: 'Gertie')
-
-      # items
-      @item1 = @merchant1.items.create!(name: 'Pogo stick', description: 'Jumpin Stick', unit_price: 100, enabled: true)
-      @item2 = @merchant1.items.create!(name: 'Yo - Yo', description: 'Goes and yos', unit_price: 100, enabled: true)
-      @item3 = @merchant1.items.create!(name: 'Rollerskates', description: 'Lets roll', unit_price: 100, enabled: true)
-      @item4 = @merchant2.items.create!(name: 'Fun Dip', description: 'Dip the fun', unit_price: 100, enabled: true)
-
-      # customers
-      @customer1 = Customer.create!(first_name: 'Sinead', last_name: 'Maguire')
-      @customer2 = Customer.create!(first_name: 'Amy', last_name: 'Russel')
-
-      # invoices
-      @invoice1 = @customer1.invoices.create!(status: 0)
-      @invoice2 = @customer1.invoices.create!(status: 0)
-      @invoice3 = @customer2.invoices.create!(status: 0)
-
-      # invoice_items
-      @invoice_item1 = InvoiceItem.create!(item_id: @item1.id, invoice_id: @invoice1.id, quantity: 10, unit_price: 1000, status: 0)
-      @invoice_item2 = InvoiceItem.create!(item_id: @item2.id, invoice_id: @invoice1.id, quantity: 10, unit_price: 2000, status: 1)
-      @invoice_item3 = InvoiceItem.create!(item_id: @item4.id, invoice_id: @invoice2.id, quantity: 10, unit_price: 100, status: 1)
-      @invoice_item4 = InvoiceItem.create!(item_id: @item3.id, invoice_id: @invoice3.id, quantity: 20, unit_price: 100, status: 1)
-    end
 
     describe '#unique_invoices' do
-      it 'can find unique invoices for a merchant' do
-        expect(@merchant1.unique_invoices).to eq([@invoice1, @invoice3])
+      it 'finds unique invoices for a merchant' do
+        expect(merchant1.unique_invoices).to eq([invoice1])
+        expect(merchant2.unique_invoices).to eq([invoice2a, invoice2b,
+                                                 invoice2c, invoice2d,
+                                                 invoice2e])
       end
     end
 
     describe '#invoice_items_for_merchant' do
-      it 'can find merchant1 invoice items for invoice1' do
-        expect(@merchant1.invoice_items_for_invoice(@invoice1.id)).to eq([@invoice_item1, @invoice_item2])
+      it 'finds merchant1 invoice items for invoice1' do
+        expect(merchant1.invoice_items_for_invoice(invoice1.id))
+          .to eq([invoice_item1a, invoice_item1b])
+        expect(merchant2.invoice_items_for_invoice(invoice2a.id))
+          .to eq([invoice_item2a, invoice_item2b])
       end
     end
   end
