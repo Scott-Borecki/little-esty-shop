e4c8fc2c4d1083a96cf2a9e3577e95ccd1b0dbd4
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-08-01 22:21:37 -0600

    Add update method to invoices controller

--- app/controllers/merchants/invoices_controller.rb
+++ app/controllers/merchants/invoices_controller.rb
@@ -8,6 +8,13 @@ class Merchants::InvoicesController < ApplicationController
     @invoice = Invoice.find(params[:id])
   end
 
+  def update
+    @invoice = Invoice.find(params[:id])
+    item = InvoiceItem.find(params[:item_id])
+    item.update(status: params[:status])
+    redirect_to("/merchants/#{@merchant.id}/invoices/#{@invoice.id}")
+  end
+
   def get_merchant
     @merchant = Merchant.find(params[:merchant_id])
   end
