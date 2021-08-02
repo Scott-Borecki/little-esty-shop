6b41c990c02cc09c4c1ee4ee589bb8d381ad8ea5
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-08-02 12:21:15 -0600

    Create invoice items controller

---
+++ app/controllers/invoice_items_controller.rb
@@ -0,0 +1,14 @@
+class InvoiceItemsController < ApplicationController
+  before_action :get_merchant
+
+  def update
+    invoice_item = InvoiceItem.find(params[:id])
+    invoice_item.update(status: params[:invoice_item][:status])
+    invoice = Invoice.find(invoice_item.invoice_id)
+    redirect_to("/merchants/#{@merchant.id}/invoices/#{invoice.id}")
+  end
+
+  def get_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+end
