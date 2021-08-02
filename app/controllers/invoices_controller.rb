4132bce9c1463421a468c40c62c7e00b29f54ec1
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-08-02 12:21:30 -0600

    Remove update method from invoices controller

---
+++ app/controllers/invoices_controller.rb
@@ -0,0 +1,14 @@
+class InvoicesController < ApplicationController
+  before_action :get_merchant
+
+  def index
+  end
+
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
+
+  def get_merchant
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+end
